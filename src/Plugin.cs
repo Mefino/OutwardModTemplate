d0f8cbd2fe23c3504d9ff2c89ad38b948a87a6d8
Sinai <49360850+sinai-dev@users.noreply.github.com>
2022-05-29 02:33:23 +1000

    Update and rename MyMod.cs to Plugin.cs

--- src/Plugin.cs
+++ src/Plugin.cs
@@ -7,29 +7,20 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Text;
 
+// RENAME 'OutwardModTemplate' TO SOMETHING ELSE
 namespace OutwardModTemplate
 {
-    /* ~~~ Initial Setup ~~~
-     * 
-     * 1. Double click on "Properties" in the Solution Explorer panel to the left
-     * 2. Click on the "Application" tab
-     * 3. Change your "Assembly Name" and "Default Namespace" to something unique for your mod.
-     * 4. Right click the namespace "OutwardModTemplate" above and rename it to what you chose for your Namespace.
-     * 5. Right click "MyMod" below and rename it, generally you use what you chose for your Assembly Name.
-     * 6. Read the rest of the comments in this file and make changes as needed.
-     */
-
     [BepInPlugin(GUID, NAME, VERSION)]
-    public class MyMod : BaseUnityPlugin
+    public class Plugin : BaseUnityPlugin
     {
         // Choose a GUID for your project. Change "myname" and "mymod".
         public const string GUID = "myname.mymod";
         // Choose a NAME for your project, generally the same as your Assembly Name.
-        public const string NAME = "MyMod";
+        public const string NAME = "My Mod";
         // Increment the VERSION when you release a new version of your mod.
         public const string VERSION = "1.0.0";
 
-        // For accessing your BepInEx Logger from outside of this class (MyMod.Log)
+        // For accessing your BepInEx Logger from outside of this class (eg Plugin.Log.LogMessage("");)
         internal static ManualLogSource Log;
 
         // If you need settings, define them like so:
